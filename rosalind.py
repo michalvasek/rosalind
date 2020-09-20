a4d4599b7fc9a418ac32051d82db3985424dacb8
Michal Vasek <michalvasek@gmail.com>
2020-09-20 04:55:17 +0200

    New solution for "dna"

--- rosalind.py
+++ rosalind.py
@@ -1,6 +1,26 @@
 import sys
 
 
+#  Title: Counting DNA Nucleotides
+#  URL:   http://rosalind.info/problems/dna/
+def dna(dataset_file):
+
+    acgt = {'A' : 0,
+            'C' : 0,
+            'G' : 0,
+            'T' : 0 }
+
+    with open(dataset_file, 'r') as file:
+        for line in file:
+            for char in line:
+                if char in acgt:
+                    acgt[char] += 1
+
+    result = '%d %d %d %d' % (acgt['A'], acgt['C'], acgt['G'], acgt['T']) 
+
+    return result
+
+
 if __name__ == '__main__':
 
     try:
