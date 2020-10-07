bf7ae0d8fbb04e177c88f3dbca9226577baf864f
Michal Vasek <michalvasek@gmail.com>
2020-10-07 23:32:43 +0200

    New solution for "prtm"

--- rosalind.py
+++ rosalind.py
@@ -352,6 +352,29 @@ def grph(dataset_file):
     return result
 
 
+#  Title: Calculating Protein Mass
+#  URL:   http://rosalind.info/problems/prtm/
+def prtm(dataset_file):
+
+    result = 0
+    weights = {'A' : 71.03711,  'C' : 103.00919, 'D' : 115.02694,
+               'E' : 129.04259, 'F' : 147.06841, 'G' : 57.02146,
+               'H' : 137.05891, 'I' : 113.08406, 'K' : 128.09496,
+               'L' : 113.08406, 'M' : 131.04049, 'N' : 114.04293,
+               'P' : 97.05276,  'Q' : 128.05858, 'R' : 156.10111,
+               'S' : 87.03203,  'T' : 101.04768, 'V' : 99.06841,
+               'W' : 186.07931, 'Y' : 163.06333 }
+
+    with open(dataset_file, 'r') as file:
+        protein = file.readline()[:-1]
+
+    for aa in protein:
+        result += weights[aa]
+
+    result = str(round(result, 3))
+    return result
+
+
 if __name__ == '__main__':
 
     try:
